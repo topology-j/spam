bc61e06f2e5d9a50454b41eb89c6b8f106e178d3
topology-j <greenenjoyment@gmail.com>
2026-04-16 22:47:35 +0900

    clean: 전체 파일 제거 (RAG 완성 후 재업로드)

--- del_spam_kw.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-db = r"C:\Users\green\Desktop\스팸데이터\backend\spam_chat.db"
-conn = sqlite3.connect(db)
-conn.execute("DELETE FROM spam_keywords WHERE keyword='스팸'")
-conn.commit()
-print("삭제 완료")
-conn.close()
